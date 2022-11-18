c0f447c33494d5b5a0ba19c4df33b08f5c917367
Joppan420 <jovan.rakic@student.htldornbinr.at>
2022-11-18 08:45:42 +0100

    String_Zahlen

---
+++ JS_Basics/02 Strings_Zahlen/script.js
@@ -0,0 +1,10 @@
+let a = 17.4;
+let b = 3.9;
+
+let ab = a / b;
+console.log("The result of " + a + " divided by " + b + " is " + ab);
+
+
+let firstname = "Jovan";
+let lastname = "Rakic";
+console.log("Ich heiße " + firstname + " " + lastname);
