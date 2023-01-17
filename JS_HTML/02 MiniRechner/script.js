9940bbb4f02ba22e4b923fb8fe69ff9b5abd5c86
Joppan420 <jovan.rakic@student.htldornbinr.at>
2023-01-17 11:52:09 +0100

    mini rechner not finished

---
+++ JS_HTML/02 MiniRechner/script.js
@@ -0,0 +1,10 @@
+function buttonClicked() {
+    console.log("clicked");
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+
+    let result = num1 + num2;
+
+    console.log(result);
+}
+
