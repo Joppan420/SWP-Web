b9a2c447638175b22d523936da9e40f461f8413c
Joppan420 <jovan.rakic@student.htldornbinr.at>
2023-01-20 09:17:09 +0100

    mini Rechner

--- JS_HTML/02 MiniRechner/script.js
+++ JS_HTML/02 MiniRechner/script.js
@@ -1,10 +1,35 @@
-function buttonClicked() {
+function addNumbers() {
     console.log("clicked");
     let num1 = document.getElementById("number1").value;
     let num2 = document.getElementById("number2").value;
+    let result = parseFloat(num1) + parseFloat(num2);
 
-    let result = num1 + num2;
-
-    console.log(result);
+    document.getElementById("output").innerHTML = result;
 }
 
+function subtractNumbers() {
+    console.log("clicked");
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+    let result = parseFloat(num1) - parseFloat(num2);
+
+    document.getElementById("output").innerHTML = result;
+}
+
+function multiplyNumbers() {
+    console.log("clicked");
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+    let result = parseFloat(num1) * parseFloat(num2);
+
+    document.getElementById("output").innerHTML = result;
+}
+
+function divideNumbers() {
+    console.log("clicked");
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+    let result = parseFloat(num1) / parseFloat(num2);
+
+    document.getElementById("output").innerHTML = result;
+}
