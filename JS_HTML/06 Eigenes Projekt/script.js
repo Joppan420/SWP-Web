ab4b26fde12ed8e3014f6902b06521481716de7d
Joppan420 <jovan.rakic@student.htldornbinr.at>
2023-03-07 13:04:57 +0100

    Eigenes Projekt weiter

--- JS_HTML/06 Eigenes Projekt/script.js
+++ JS_HTML/06 Eigenes Projekt/script.js
@@ -29,3 +29,19 @@ function lira() {
 
     document.getElementById("output").innerHTML = result;
 }
+
+
+
+var myHeaders = new Headers();
+myHeaders.append("apikey", "JA0TPvK0R9mcGCPnG8HXSt9sXdzZHzii");
+
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+fetch("https://api.apilayer.com/exchangerates_data/convert?to=USD&from=EUR&amount=15", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
