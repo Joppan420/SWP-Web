cd0a505ded55ab319d082c934c59a477ea3c939b
Joppan420 <jovan.rakic@student.htldornbinr.at>
2023-03-10 09:28:13 +0100

    Eigenes Projekt mit APIs angefangen

--- JS_HTML/06 Eigenes Projekt/script.js
+++ JS_HTML/06 Eigenes Projekt/script.js
@@ -1,47 +1,61 @@
-function dollar() {
-    console.log("clicked");
-    let num1 = document.getElementById("number1").value;
-    let result = parseFloat(num1) * 1.06;
+let dollarAvailable = false;
+let dollarRate = 1;
 
-    document.getElementById("output").innerHTML = result;
+function dollar() {
+  if (!dollarAvailable){
+    alert("no wechselkurs");
+    return;
+  }
+  
+  
+  console.log("clicked");
+  let num1 = document.getElementById("number1").value;
+  let result = parseFloat(num1) * dollarRate;
+
+  document.getElementById("output").innerHTML = result + " rate:" + dollarRate;
 }
 
 function yen() {
-    console.log("clicked");
-    let num1 = document.getElementById("number1").value; 
-    let result = parseFloat(num1) * 145.16;
+  console.log("clicked");
+  let num1 = document.getElementById("number1").value;
+  let result = parseFloat(num1) * 145.16;
 
-    document.getElementById("output").innerHTML = result;
+  document.getElementById("output").innerHTML = result;
 }
 
 function dinar() {
-    console.log("clicked");
-    let num1 = document.getElementById("number1").value;
-    let result = parseFloat(num1) * 117.30;
+  console.log("clicked");
+  let num1 = document.getElementById("number1").value;
+  let result = parseFloat(num1) * 117.3;
 
-    document.getElementById("output").innerHTML = result;
+  document.getElementById("output").innerHTML = result;
 }
 
 function lira() {
-    console.log("clicked");
-    let num1 = document.getElementById("number1").value;
-    let result = parseFloat(num1) * 20.05;
+  console.log("clicked");
+  let num1 = document.getElementById("number1").value;
+  let result = parseFloat(num1) * 20.05;
 
-    document.getElementById("output").innerHTML = result;
+  document.getElementById("output").innerHTML = result;
 }
-
-
 
 var myHeaders = new Headers();
 myHeaders.append("apikey", "JA0TPvK0R9mcGCPnG8HXSt9sXdzZHzii");
 
 var requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-  headers: myHeaders
+  method: "GET",
+  redirect: "follow",
+  headers: myHeaders,
 };
 
-fetch("https://api.apilayer.com/exchangerates_data/convert?to=USD&from=EUR&amount=15", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+fetch(
+  "https://api.apilayer.com/exchangerates_data/convert?to=USD&from=EUR&amount=15",
+  requestOptions
+)
+  .then((response) => response.json())
+  .then((result) => {
+    console.log(result);
+    dollarRate = result.info.rate;
+    dollarAvailable = true;
+  })
+  .catch((error) => console.log("error", error));
