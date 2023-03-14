4b3506c5697d0b7d48e5596cdb8d8b010d30fce8
Joppan420 <jovan.rakic@student.htldornbinr.at>
2023-03-14 10:53:23 +0100

    Eigenes Projekt mit API

--- JS_HTML/06 Eigenes Projekt/script.js
+++ JS_HTML/06 Eigenes Projekt/script.js
@@ -1,61 +1,30 @@
-let dollarAvailable = false;
-let dollarRate = 1;
-
-function dollar() {
-  if (!dollarAvailable){
-    alert("no wechselkurs");
-    return;
-  }
-  
-  
-  console.log("clicked");
-  let num1 = document.getElementById("number1").value;
-  let result = parseFloat(num1) * dollarRate;
-
-  document.getElementById("output").innerHTML = result + " rate:" + dollarRate;
-}
-
-function yen() {
-  console.log("clicked");
-  let num1 = document.getElementById("number1").value;
-  let result = parseFloat(num1) * 145.16;
-
-  document.getElementById("output").innerHTML = result;
-}
-
-function dinar() {
-  console.log("clicked");
-  let num1 = document.getElementById("number1").value;
-  let result = parseFloat(num1) * 117.3;
-
-  document.getElementById("output").innerHTML = result;
-}
-
-function lira() {
-  console.log("clicked");
-  let num1 = document.getElementById("number1").value;
-  let result = parseFloat(num1) * 20.05;
-
-  document.getElementById("output").innerHTML = result;
-}
-
-var myHeaders = new Headers();
+const myHeaders = new Headers();
 myHeaders.append("apikey", "JA0TPvK0R9mcGCPnG8HXSt9sXdzZHzii");
 
-var requestOptions = {
-  method: "GET",
-  redirect: "follow",
-  headers: myHeaders,
-};
+const signs = {
+  ["JPY"]: "¥",
+  ["USD"]: "$",
+  ["RSD"]: "Din.",
+  ["TRY"]: "₺"
+}
 
-fetch(
-  "https://api.apilayer.com/exchangerates_data/convert?to=USD&from=EUR&amount=15",
-  requestOptions
-)
-  .then((response) => response.json())
-  .then((result) => {
-    console.log(result);
-    dollarRate = result.info.rate;
-    dollarAvailable = true;
-  })
-  .catch((error) => console.log("error", error));
+function convert(convertData) {
+  const input = parseFloat(document.getElementById("number1").value);
+
+  fetch(
+    `https://api.apilayer.com/exchangerates_data/convert?to=${convertData.id}&from=EUR&amount=${input}`,
+    {
+      method: "GET",
+      redirect: "follow",
+      headers: myHeaders,
+    }
+    )
+    .then((response) => response.json())
+    .then(({ result }) => {
+      console.log(result)
+      document.getElementById(
+        "output"
+      ).innerHTML = `The converted amount was ${result} ${signs[convertData.id]}`;
+    })
+    .catch((error) => console.log("error", error));
+}
